f3508b5a3f5da12252257262cc075dcca301ddfd
Justin Zhang <schnell18@gmail.com>
2015-04-02 15:38:50 +0800

    Add auto-build files for CentOS 7.1

--- centos/build.bat
+++ centos/build.bat
@@ -3,5 +3,5 @@ set PACKER_LOG_PATH=packer_err.log
 REM packer build 5/centos-5.11-x86_64.json
 REM packer build 6/centos-6.5-x86_64.json
 REM packer build 6/centos-6.6-x86_64.json
-packer build 6/centos-6.6-java7-x86_64.json
-REM packer build 7/centos-7.0.json
+REM packer build 6/centos-6.6-java7-x86_64.json
+packer build 7/centos-7.1.json
