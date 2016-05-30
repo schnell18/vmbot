b2183e09117173dbac92b6ec26504682d9bae016
Justin Zhang <schnell18@gmail.com>
2016-05-30 09:09:59 +0800

    打通devel到其它虚拟机的ssh权限
    - 使用淘宝npm源安装node

--- centos/build.sh
+++ centos/build.sh
@@ -5,6 +5,7 @@ export PACKER_LOG_PATH=packer_err.log
 # packer build 6/centos-6.6-x86_64.json
 # packer build 6/centos-6.6-java7_dev-x86_64.json
 # packer build 6/centos-6.6-java8_dev-x86_64.json
-packer build 6/centos-6.7-x86_64.json
+packer build 6/centos-6.6-java8-node_dev-x86_64.json
+# packer build 6/centos-6.7-x86_64.json
 # packer build 6/centos-6.6-java7-x86_64.json
 # packer build 7/centos-7.0.json
