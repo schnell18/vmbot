15b7c8af942b26db506d0c6728628e177b876487
Justin Zhang <fgz@qad.com>
2018-05-05 22:14:49 +0800

    Add centos 7.4

--- centos/build.sh
+++ centos/build.sh
@@ -4,5 +4,7 @@ export PACKER_LOG_PATH=packer_err.log
 # packer build 6/centos-6.5-x86_64.json
 # packer build 6/centos-6.6-x86_64.json
 # packer build 6/centos-6.6-java7-x86_64.json
-packer build 6/centos-6.9-java8-x86_64.json
-# packer build 7/centos-7.0.json
+# packer build 6/centos-6.9-java8-x86_64.json
+# packer build 6/centos-6.9-x86_64.json
+# # packer build 7/centos-7.0.json
+packer build 7/centos-7.4.json
