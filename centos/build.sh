f1e789611ed8fbedc1e862453b35aab98c9ad717
Justin Zhang <fgz@qad.com>
2018-05-05 20:42:01 +0800

    Add centos 6.9 and java 8 box build files

--- centos/build.sh
+++ centos/build.sh
@@ -3,9 +3,15 @@ export PACKER_LOG_PATH=packer_err.log
 # packer build 5/centos-5.11-x86_64.json
 # packer build 6/centos-6.5-x86_64.json
 # packer build 6/centos-6.6-x86_64.json
+<<<<<<< HEAD
 # packer build 6/centos-6.6-java7_dev-x86_64.json
 # packer build 6/centos-6.8-java8_dev-x86_64.json
 # packer build 6/centos-6.8-java8-node_dev-x86_64.json
 # packer build 6/centos-6.7-x86_64.json
 # packer build 6/centos-6.6-java7-x86_64.json
 packer build 7/centos-7.3.json
+=======
+# packer build 6/centos-6.6-java7-x86_64.json
+packer build 6/centos-6.9-java8-x86_64.json
+# packer build 7/centos-7.0.json
+>>>>>>> Add centos 6.9 and java 8 box build files
