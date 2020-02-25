1d0cce6318a963781b7c5db78aa4c5ab989020d7
JustinZhang <schnell18@gmail.com>
2020-02-25 16:30:26 +0800

    Add debian 10.3 and SATA controller support
    - Change system disk from IDE controller to SATA controller
    - Install packages to enable apt works w/ HTTPS

--- debian/build.sh
+++ debian/build.sh
@@ -3,4 +3,7 @@ export PACKER_LOG_PATH=packer_err.log
 #packer build -debug 9/debian-9.8.json
 #packer build 9/debian-9.8.json
 #packer build 9/debian-9.9.json
-packer build 10/debian-10.json
+#packer build 10/debian-10.json
+#packer build 10/debian-10.2.json
+#packer build -debug 10/debian-10.3.json
+packer build 10/debian-10.3.json
