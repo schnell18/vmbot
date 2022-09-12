0c150c58fbd22d422a98f90f971005011945c1af
Justin Zhang <schnell18@gmail.com>
2022-09-12 22:22:51 +0800

    Add rocky linux 9

--- rockylinux/build.sh
+++ rockylinux/build.sh
@@ -1,4 +1,5 @@
 export PACKER_LOG=1
 export PACKER_LOG_PATH=packer_err.log
 # packer build 8/rockylinux-8.3.json
-packer build 8/rockylinux-8.4.json
+# packer build 8/rockylinux-8.4.json
+packer build 9/rockylinux-9.0.json
