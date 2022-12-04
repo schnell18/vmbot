24c199cb194a59e3af2a0c41d095fb2cf796b8cc
Justin Zhang <schnell18@gmail.com>
2022-12-04 23:20:20 +0800

    Fix vboxsf not loading error
    - Add simple unit tests

--- rocky/build.sh
+++ rocky/build.sh
@@ -1,6 +1,7 @@
 export PACKER_LOG=1
 export PACKER_LOG_PATH=packer_err.log
-# packer build 8/rockylinux-8.3.json
-# packer build 8/rockylinux-8.4.json
+# packer build 9/rockylinux-9.1.json
 # packer build 9/rockylinux-9.0.json
 packer build 8/rockylinux-8.7.json
+# packer build 8/rockylinux-8.5.json
+# packer build 8/rockylinux-8.4.json
